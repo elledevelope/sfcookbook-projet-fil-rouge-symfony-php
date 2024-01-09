04c9cf8fa4ebe17bc0b4bbfc8daff71ac2e756f5
Elmira PETIT <elledevelope@gmail.com>
2024-01-09 15:04:40 +0100

    Update db: removed all changes, it's back to it's initial state

---
+++ migrations/Version20240109133542.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240109133542 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipes ADD recipe_id INT NOT NULL, CHANGE id id INT AUTO_INCREMENT NOT NULL, ADD PRIMARY KEY (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipes MODIFY id INT NOT NULL');
+        $this->addSql('DROP INDEX `primary` ON recipes');
+        $this->addSql('ALTER TABLE recipes DROP recipe_id, CHANGE id id INT NOT NULL');
+    }
+}
