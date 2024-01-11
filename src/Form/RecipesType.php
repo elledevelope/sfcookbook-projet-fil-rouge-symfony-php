7fd30cf461a42bd6f88fa74c176190f2e60cd513
Elmira PETIT <elledevelope@gmail.com>
2024-01-11 12:05:13 +0100

    Added placeholders and hints for texarea form in RecipesType.php

--- src/Form/RecipesType.php
+++ src/Form/RecipesType.php
@@ -3,12 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Recipes;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Validator\Constraints\Length;
 
 class RecipesType extends AbstractType
 {
@@ -16,10 +18,56 @@ class RecipesType extends AbstractType
     {
         $builder
             // ->add('recipe_id')
-            ->add('title')
+
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'constraints' => [
+                    new Length([
+                        'min' => 2, 
+                        'minMessage' => 'The title must be at least {{ limit }} characters!',
+                        'max' => 20,
+                        'maxMessage' => 'The title cannot be longer than {{ limit }} characters!',
+                    ]),
+                ],
+            ])
+
             ->add('description')
-            ->add('ingredients')
-            ->add('instructions')
+
+            ->add('ingredients', TextareaType::class, [
+                'label' => 'Ingredients',
+                
+                'attr' => [
+                    'placeholder' => 'example: 
+Preheat your oven to 220°C;
+Add sliced onions;
+... ;',
+                    'class' => 'form-control', // Add your form control class if needed
+                    'rows' => 10,
+                    'cols' => 50,
+                ],
+                'help' => 'Please separate ingredients with a semicolon (;).',
+                'help_attr' => [
+                    'class' => 'small-help-text',
+                ],
+            ])
+
+            ->add('instructions', TextareaType::class, [
+                'label' => 'Instructions',
+                'attr' => [
+                    'placeholder' => 'example: 
+700g boneless chicken; 
+1/4 cup olive oil;
+...;',
+                    'class' => 'form-control', // Add your form control class if needed
+                    'rows' => 10,
+                    'cols' => 50,
+                ],
+                'help' => 'Please separate instructions with a semicolon (;).',
+                'help_attr' => [
+                    'class' => 'small-help-text',
+                ],
+            ])
+
 
             // Use ChoiceType as radio buttons
             ->add('level', ChoiceType::class, [
@@ -51,7 +99,7 @@ class RecipesType extends AbstractType
                 'label' => 'Image',
                 'required' => true, // true if the image is mandatory
                 'data_class' => null, // Allow handling as array
-            ])            
+            ])
 
             // ->add('user_id')
             // ->add('created_at')
