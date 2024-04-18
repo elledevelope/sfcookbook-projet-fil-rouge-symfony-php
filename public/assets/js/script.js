ba12689498d1c4bc12714a70720d18d90cccf08a
Elmira PETIT <elledevelope@gmail.com>
2024-04-18 15:04:18 +0200

    Fix bug with toggle on 'add to favorite' icon on page 'recipes'

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -18,14 +18,33 @@ closeIcon.addEventListener('click', () => {
 
 
 // --------------- Add / Remove favorite recipe:
-$('.like-icon').click(function () {
+$('.like-icon').on('click', function (event) {
+    // Ensure the event is triggered by the .like-icon div or its children
+    if (!$(event.target).closest('.like-icon').length) {
+        return; // Ignore clicks on other elements
+    }
+
     const $icon = $(this).find('i');
     const $text = $(this).find('p');
     const recipeId = $(this).data('recipe-id');
     const isFavorite = $(this).hasClass('clicked');
 
+    // Toggle the 'clicked' class on the like-icon
+    // $(this).toggleClass('clicked');
+
     // Toggle visibility of the text
     $text.toggle();
+
+    // Toggle between solid and outline heart icons
+    if ($icon.hasClass('bx-bookmark-heart')) {
+        $icon.removeClass('bx-bookmark-heart').addClass('bxs-bookmark-heart');
+    } else {
+        $icon.removeClass('bxs-bookmark-heart').addClass('bx-bookmark-heart');
+    }
+
+    // Ensure style is applied before AJAX request
+    // Trigger re-render or refresh
+    $text.css('opacity', '0').css('opacity', '1');
 
     // Endpoint based on whether the recipe is being added or removed from favorites
     const endpoint = isFavorite ? '/remove-from-favorites/' : '/add-to-favorites/';
@@ -36,16 +55,6 @@ $('.like-icon').click(function () {
         type: isFavorite ? 'DELETE' : 'POST',
         success: (response) => {
             if (response.success) {
-                // Toggle the 'clicked' class on the like-icon
-                $(this).toggleClass('clicked');
-
-                // Explicitly add or remove classes to ensure correct icon is displayed
-                if (isFavorite) {
-                    $icon.removeClass('bx-bookmark-heart').addClass('bxs-bookmark-heart');
-                } else {
-                    $icon.removeClass('bxs-bookmark-heart').addClass('bx-bookmark-heart');
-                }
-
                 if (isFavorite) {
                     // alert('Recipe removed from favorites!');
                 } else {
@@ -60,3 +69,6 @@ $('.like-icon').click(function () {
         }
     });
 });
+
+
+
